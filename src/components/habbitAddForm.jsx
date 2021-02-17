fe542af4e19b0089caced36826ca5dd65c5bc59a
Jaejuna <anayana9988@gmail.com>
2021-02-17 19:30:02 +0900

    habbit tracker done

--- src/components/habbitAddForm.jsx
+++ src/components/habbitAddForm.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class HabbitAddForm extends Component {
+class HabbitAddForm extends PureComponent {
   formRef = React.createRef();
   inputRef = React.createRef();
 
